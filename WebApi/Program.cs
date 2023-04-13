455c6409965444562bcd9e5e236b77672d5b9ecf
Tomasz Sikora <tomasz.sikora@snowsoftware.com>
2023-04-13 19:47:48 +0200

    new credentials

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,10 +15,10 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     {
         options.LoginPath = "/Auth/signin";
     })
-    .AddGoogle("Google", options =>
+    .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
     {
-        options.ClientId = "450141675492-9rhakm7lfrvsh846f674b5rrd656q983.apps.googleusercontent.com";
-        options.ClientSecret = "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";
+        options.ClientId = "269492224424-9oag6eaooneq3fu8j2hu7qops3d8i5c0.apps.googleusercontent.com";
+        options.ClientSecret = "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";
         options.CallbackPath = "/signin-google"; 
     });
 
