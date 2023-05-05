6279cda7a6ba54c041c232f92e0e38347bf7312c
Tomasz Sikora <sikora507@gmail.com>
2023-05-05 18:36:10 +0200

    adding authentication

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,6 +6,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication()
+    .AddGoogle("Google", options =>
+    {
+        options.ClientId = "";
+        options.ClientSecret = "";
+        options.CallbackPath = "";
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +26,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
