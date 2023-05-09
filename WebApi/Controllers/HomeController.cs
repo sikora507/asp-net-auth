673977fad1f6dca8a45197f7fd3844bd300fcddc
Tomasz Sikora <sikora507@gmail.com>
2023-05-09 13:46:07 +0200

    getting user info

--- WebApi/Controllers/HomeController.cs
+++ WebApi/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,13 +11,15 @@ public class HomeController : ControllerBase
     [HttpGet]
     public IActionResult Index()
     {
-        return Ok(new { Message = "Index route in HomeController" });
+        var userEmail = HttpContext.User.FindFirst(ClaimTypes.Email)?.Value;
+        return Ok(new { Message = "Index route in HomeController", Email = userEmail });
     }
     
     [Authorize]
     [HttpGet("/protected")]
     public IActionResult Protected()
     {
-        return Ok(new { Message = "This is a protected route" });
+        var userEmail = HttpContext.User.FindFirst(ClaimTypes.Email)?.Value;
+        return Ok(new { Message = "This is a protected route", Email = userEmail });
     }
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,7 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.local.json", true);
@@ -19,6 +21,24 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.ClientSecret = builder.Configuration.GetValue<string>("Authentication:Google:ClientSecret") ?? "";
         options.CallbackPath = "/signin-google"; 
         options.AuthorizationEndpoint += "?prompt=select_account";
+        options.Events = new OAuthEvents
+        {
+            OnTicketReceived = async context =>
+            {
+                // You can access the user's email here
+                var email = context.Principal.FindFirstValue(ClaimTypes.Email);
+    
+                // Store the email in the user's claims
+                var claims = new List<Claim>
+                {
+                    new Claim("Email", email)
+                };
+                var appIdentity = new ClaimsIdentity(claims);
+                context.Principal.AddIdentity(appIdentity);
+                await Task.CompletedTask;
+            }
+        };
+
     });
 builder.Services.AddAuthorization();
 
